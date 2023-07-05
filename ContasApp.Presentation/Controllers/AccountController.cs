c4cb7ac35d88f97d9a7030031aba92f719f67299
smendescoti <sergio.coti@gmail.com>
2023-07-05 13:14:04 -0300

    feat: Desenvolvimento do cadastro de conta de usuário

--- ContasApp.Presentation/Controllers/AccountController.cs
+++ ContasApp.Presentation/Controllers/AccountController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ContasApp.Data.Entities;
+using ContasApp.Data.Repositories;
+using ContasApp.Presentation.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ContasApp.Presentation.Controllers
 {
@@ -20,6 +23,41 @@ namespace ContasApp.Presentation.Controllers
         /// </summary>
         public IActionResult Register()
         {
+            return View();
+        }
+
+        /// <summary>
+        /// Método para capturar o SUBMIT POST da página /Account/Register
+        /// </summary>
+        [HttpPost] //Receber o SUBMIT POST do formulário
+        public IActionResult Register(AccountRegisterViewModel model)
+        {
+            //verificar se todos os campos passaram nas regras de validação
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    //capturando os dados do usuário
+                    var usuario = new Usuario();
+
+                    usuario.Id = Guid.NewGuid();
+                    usuario.Nome = model.Nome;
+                    usuario.Email = model.Email;
+                    usuario.Senha = model.Senha;
+                    usuario.DataHoraCriacao = DateTime.Now;
+
+                    //gravando o usuário no banco de dados
+                    var usuarioRepository = new UsuarioRepository();
+                    usuarioRepository.Add(usuario);
+
+                    TempData["Mensagem"] = "Parabéns, sua conta de usuário foi cadastrada com sucesso!";
+                }
+                catch(Exception e)
+                {
+                    TempData["Mensagem"] = e.Message;
+                }
+            }
+
             return View();
         }
 
---
+++ ContasApp.Presentation/Models/AccountRegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContasApp.Presentation.Models
+{
+    /// <summary>
+    /// Modelo de dados para a página de criação de conta de usuário
+    /// </summary>
+    public class AccountRegisterViewModel
+    {
+        [MaxLength(150, ErrorMessage = "Por favor, informe no máximo {1} caracteres.")]
+        [MinLength(8, ErrorMessage = "Por favor, informe no mínimo {1} caracteres.")]
+        [Required(ErrorMessage = "Por favor, informe o nome do usuário.")]
+        public string? Nome { get; set; }
+
+        [EmailAddress(ErrorMessage = "Por favor, informe um endereço de email válido.")]
+        [Required(ErrorMessage = "Por favor, informe o email do usuário.")]
+        public string? Email { get; set; }
+
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$", 
+            ErrorMessage = "Informe uma senha com letras maísculas, letras minúsculas, números, símbolos e no mínimo 8 caracteres.")]
+        [Required(ErrorMessage = "Por favor, informe a senha do usuário.")]
+        public string? Senha { get; set; }
+
+        [Compare("Senha", ErrorMessage = "Senhas não conferem, por favor verifique.")]
+        [Required(ErrorMessage = "Por favor, confirme a senha do usuário.")]
+        public string? SenhaConfirm { get; set; }
+    }
+}
